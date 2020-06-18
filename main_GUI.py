2d7cad08f6de642cfe2dee324cfe1ebf86d1e4bc
Lingzhi <ncllzsony@gmail.com>
2020-06-18 16:47:12 +0200

    pic format conversion and small .grid adjustment

--- main_GUI.py
+++ main_GUI.py
@@ -8,7 +8,7 @@ from tkinter import filedialog
 from nltk import *
 from urllib import request
 from bigram_generator import BigramGenerator
-
+from tkinter import ttk
 
 def createpage(master):
     def selectPath():
@@ -49,14 +49,14 @@ def createpage(master):
 
         # TopLevel Window
         window_local = tk.Toplevel(root)
-        window_local.geometry('550x200')
+        window_local.geometry('500x50')
         window_local.title('Local File')
 
         filename = StringVar()
 
-        Label(window_local, text="Select your txt file:").place(x=20, y=120)
-        Entry(window_local, textvariable=filename).place(x=230, y=120)
-        Button(window_local, text="Select and Submit", command=opentxt).place(x=400, y=120)
+        Label(window_local, text="Select your txt file:").grid(row=0, column=0, stick=W)
+        Entry(window_local, textvariable=filename).grid(row=0, column=1, stick=W)
+        ttk.Button(window_local, text="Select and Submit", command=opentxt).grid(row=0, column=2, stick=E)
 
     # URL Link Submission
     def url_submisson():
@@ -88,15 +88,15 @@ def createpage(master):
 
         # TopLevel Window
         window_link = tk.Toplevel(root)
-        window_link.geometry('650x200')
+        window_link.geometry('600x50')
         window_link.title('URL Link')
 
         url = tk.StringVar()  # give the link to the string value
         url.set('http://www.gutenberg.org/files/2554/2554-0.txt')
 
-        Label(window_link, text='URL Link: ').place(x=10, y=130)
-        Entry(window_link, textvariable=url, width=40).place(x=230, y=130)
-        Button(window_link, text="Submit", command=confirm_submission).place(x=550, y=130)
+        Label(window_link, text='URL Link: ').grid(row=0, column=0, stick=W)
+        Entry(window_link, textvariable=url, width=40).grid(row=0, column=1, stick=W)
+        ttk.Button(window_link, text="Submit", command=confirm_submission).grid(row=0, column=2, stick=E)
 
     master = Frame(root)
     master.pack()
@@ -104,15 +104,15 @@ def createpage(master):
     Label(master, text="1.Select your Target Directory:").grid(row=0, column=0, stick=W, pady=10)
     e1 = Entry(master, textvariable=path, width=30)
     e1.grid(row=0, column=1, stick=W)
-    Button(master, text="Select Directory", command=selectPath).grid(row=0, column=4,stick=E)
+    ttk.Button(master, text="Select Directory", command=selectPath).grid(row=0, column=4,stick=E)
 
     Label(master, text="2.Choose your way to upload the text:").grid(row=1, column=0, stick=W, pady=10)
-    Button(master,text='Local File', command = local_submission).grid(row=1, column=3, stick=E)
-    Button(master,text='URL Link', command = url_submisson).grid(row=1, column=4, stick=E)
+    ttk.Button(master,text='Local File', command = local_submission).grid(row=1, column=3, stick=E)
+    ttk.Button(master,text='URL Link', command = url_submisson).grid(row=1, column=4, stick=E)
 
     Label(master, text="3.Crawl and store bigrams:").grid(row=2, column=0, stick=W, pady=10)
     # Button传递参数
-    Button(master,text='Crawl Bigrams', command=lambda: BigramGenerator(e1.get())).grid(row=2, column=4, stick=E)
+    ttk.Button(master,text='Crawl Bigrams', command=lambda: BigramGenerator(e1.get())).grid(row=2, column=4, stick=E)
 
 
 
@@ -120,10 +120,10 @@ def createpage(master):
 if __name__ == '__main__':
     root = Tk()
     root.title('BigramCrawler')
-    root.geometry('630x550')
+    root.geometry('800x550')
     # wellcome image
     canvas = tk.Canvas(root, width=600, height=300, bg='green')
-    image_file = tk.PhotoImage(file='bigram.png')
+    image_file = tk.PhotoImage(file='bigram.gif')
     image = canvas.create_image(200, 0, anchor='n', image=image_file)
     canvas.pack(side='top')
     tk.Label(root,
